2c32fc256b35270dcf0d0c030d7af7d020572f8b
andrewljc0801 <liu.5172@osu.edu>
2021-05-04 05:51:27 -0400

    Fix Bugs

--- Lab 6/Gomoku_Player.py
+++ Lab 6/Gomoku_Player.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as mqtt
 import uuid
 import qwiic_joystick
+import time
 
 myJoystick = qwiic_joystick.QwiicJoystick()
 if myJoystick.is_connected() == False:
@@ -18,31 +19,54 @@ client.tls_set()
 # this is the username and pw we have setup for the class
 client.username_pw_set('idd', 'device@theFarm')
 
-
 #connect to the broker
 client.connect(
     'farlab.infosci.cornell.edu',
     port=8883)
 
+
 print(f"Now, you are playing {topic}!")
 player_number = input(">> Enter your player ID: ")
+send_play = play_position + " " + "0" + " " + str(player_number)
+client.publish(topic, send_play)
 while True:
     play_here = 0
-    if myJoystick.get_horizontal() == 1023:
+    if myJoystick.get_horizontal() == 0:
         if (x_position + 40) <= 680:
             x_position += 40
-    if myJoystick.get_horizontal() == 0:
+            play_position_value = (x_position, y_position)
+            play_position = str(x_position) + " " + str(y_position)
+            send_play = play_position + " " + str(play_here) + " " + str(player_number)
+            client.publish(topic, send_play)
+            time.sleep(1.2)
+    if myJoystick.get_horizontal() == 1023:
         if (x_position - 40) >= 120:
             x_position -= 40
-    if myJoystick.get_Vertical() == 1023:
+            play_position_value = (x_position, y_position)
+            play_position = str(x_position) + " " + str(y_position)
+            send_play = play_position + " " + str(play_here) + " " + str(player_number)
+            client.publish(topic, send_play)
+            time.sleep(1.2)
+    if myJoystick.get_vertical() == 0:
         if (y_position + 40) <= 680:
             y_position += 40
-    if myJoystick.get_Vertical() == 0:
+            play_position_value = (x_position, y_position)
+            play_position = str(x_position) + " " + str(y_position)
+            send_play = play_position + " " + str(play_here) + " " + str(player_number)
+            client.publish(topic, send_play)
+            time.sleep(1.2)
+    if myJoystick.get_vertical() == 1023:
         if (y_position - 40) >= 120:
             y_position -= 40
-    play_position = (x_position, y_position)
-    print("Current Position: ", play_position)
+            play_position_value = (x_position, y_position)
+            play_position = str(x_position) + " " + str(y_position)
+            send_play = play_position + " " + str(play_here) + " " + str(player_number)
+            client.publish(topic, send_play)
+            time.sleep(1.2)    
     if myJoystick.get_button() == 0:
         play_here = 1
-    send_play = play_position + " " + play_here + " " + player_number
-    client.publish(topic, send_play)
+        send_play = play_position + " " + str(play_here) + " " + str(player_number)
+        client.publish(topic, send_play)
+        time.sleep(1.5)
+    
+    
